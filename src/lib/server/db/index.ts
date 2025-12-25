98a55df361db1815927fd6ff801ee22dc70ac08c
ab22375 <ab22375@gmail.com>
2025-12-25 21:39:33 +0100

    upd

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,10 +1,11 @@
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 import * as schema from './schema';
+import { env } from '$env/dynamic/private';
 
 const client = postgres({
-	host: '/tmp',
-	database: 'translations',
-	username: 'ab'
+	host: env.DB_HOST ?? '/tmp',
+	database: env.DB_NAME ?? 'translations',
+	username: env.DB_USER ?? 'ab'
 });
 export const db = drizzle(client, { schema });
